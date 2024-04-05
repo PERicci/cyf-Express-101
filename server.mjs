02aeb6d755e996cef721d8d8426a0d43b3845ce6
Pedro <riccip@outlook.com>
2024-04-05 12:28:50 +0100

    node --watch added in script, chocolate path with amount param

--- server.mjs
+++ server.mjs
@@ -4,8 +4,25 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Yay Node!');
-  console.log(req);
+  let searchQuery = req.query.search;
+  res.send("Hello World! You searched for " + searchQuery);
+})
+
+app.get('/chocolate', (req, res) => {
+  let chocolateAmount = parseInt(req.query.amount);
+  if (isNaN(chocolateAmount) || chocolateAmount < 1) {
+    res.send('Please provide a number bigger than 0 for the amount of chocolate.');
+    return;
+  }
+  res.send(`Gimme ${chocolateAmount} chocolate${chocolateAmount === 1 ? '' : 's'}`);
+})
+
+app.get('/node', (req, res) => {
+  res.send('Node is great!');
+})
+
+app.get('/codeyourfuture', (req, res) => {
+  res.send('CodeYourFuture is the best!');
 })
 
 app.listen(port, () => {
