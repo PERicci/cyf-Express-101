9858775e4e9ade3074966c1a6814ebb669e2b59e
Pedro <riccip@outlook.com>
2024-04-05 12:33:37 +0100

    added json and multiply endpoints

--- server.mjs
+++ server.mjs
@@ -17,6 +17,22 @@ app.get('/chocolate', (req, res) => {
   res.send(`Gimme ${chocolateAmount} chocolate${chocolateAmount === 1 ? '' : 's'}`);
 })
 
+app.get("/json", (req, res) => {
+  let lat = req.query.lat;
+  let lng = req.query.lng;
+  res.send(`You searched for Lat: ${lat} and Lng: ${lng}`);
+});
+
+app.get("/multiply", (req, res) => {
+  let value1 = req.query.value1;
+  let value2 = req.query.value2;
+  if (isNaN(value1) || isNaN(value2)) {
+    res.send('Please provide numbers for both values.');
+    return;
+  }
+  res.send(`The result of ${value1} multiplied by ${value2} is ${value1 * value2}`);
+});
+
 app.get('/node', (req, res) => {
   res.send('Node is great!');
 })
