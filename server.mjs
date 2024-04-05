b08177dfd5aa3fb0c39cad7cfe47f51ffe61de38
Pedro <riccip@outlook.com>
2024-04-05 12:00:38 +0100

    app.get added

--- server.mjs
+++ server.mjs
@@ -3,6 +3,11 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+app.get('/', (req, res) => {
+  res.send('Yay Node!');
+  console.log(req);
+})
+
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}. Ready to accept requests!`);
 })
